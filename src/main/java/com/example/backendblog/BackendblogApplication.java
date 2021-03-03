9fae66b10ae2914af1aab7962bb251798e3d19fa
rsmik <r.s.mikhel@gmail.com>
2021-03-03 09:30:49 +0300

    init project

---
+++ src/main/java/com/example/backendblog/BackendblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendblogApplication.class, args);
+    }
+
+}
